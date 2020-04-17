986a85ad1232ad4bf5c6a66de13f60904b4d3760
Deer5000 <khidrbrinkley32@gmail.com>
2020-04-16 21:31:21 -0400

    All tests passed

--- strings.py
+++ strings.py
@@ -4,23 +4,49 @@ def contains(text, pattern):
     """Return a boolean indicating whether pattern occurs in text."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # TODO: Implement contains here (iteratively and/or recursively)
+    if find_index(text, pattern) is not None:
+        return True
+    else:
+        return False
 
 
-def find_index(text, pattern):
+def find_index(text, pattern, index = 0, p_letter = 0):
     """Return the starting index of the first occurrence of pattern in text,
     or None if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # TODO: Implement find_index here (iteratively and/or recursively)
-
+    if pattern =="":
+        return index
+    while index < len(text):
+        if text[index] == pattern[p_letter]:
+            if p_letter == len(pattern) - 1:
+                return index - p_letter
+            i = find_index(text,pattern, index + 1, p_letter + 1)
+            if i is not None:
+                return i
+        elif p_letter > 0:
+            return None
+        index += 1
+    return None
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
     or an empty list if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    indexes = []
+    i = find_index(text,pattern)
+    if i is not None:
+        indexes.append(i)
+    else:
+        return indexes
+    for index in indexes:
+        i = find_index(text, pattern, index + 1)
+        if i is not None and i < len(text):
+            indexes.append(i)
+        else:
+            break
+    return indexes
 
 
 def test_string_algorithms(text, pattern):
